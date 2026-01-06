f694894c03cf7a77ba3c0c7316bea97d5a0b2b61
Pvouh <kelvinkibirupeter1531@gmail.com>
2026-01-06 09:04:45 +0300

    fix: remove unnecessary eslint disable comment for image element in LoginPage

--- src/app/(dashboard)/login/page.tsx
+++ src/app/(dashboard)/login/page.tsx
@@ -20,7 +20,7 @@ export default function LoginPage() {
       <div className="w-full max-w-md">
         <div className="text-center mb-8 flex flex-col items-center">
           <div className="relative w-24 h-24 mb-4">
-             {/* eslint-disable-next-line @next/next/no-img-element */}
+           
              <img src="/images/logo.png" alt="FGCK Logo" className="object-contain w-full h-full" />
           </div>
         </div>
