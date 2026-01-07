a37a6aea4d86d7dab7cb9eecfd60758945b8bd53
Pvouh <kelvinkibirupeter1531@gmail.com>
2026-01-07 20:35:39 +0300

    Add robots.txt and sitemaps for SEO optimization
    - Created robots.txt to allow all user agents and specify the host and sitemap location.
    - Added sitemap-0.xml containing URLs for key site pages with last modified dates and change frequency.
    - Created sitemap.xml to index the sitemap-0.xml for better search engine crawling.

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.fgck-githurai44.com',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  exclude: ['/dashboard', '/dashboard/*', '/login'],
+};
